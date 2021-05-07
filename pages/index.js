906f5e0cfab63613e8d794615e990d5b4d460686
Vignesh KRV <vignesh1105krv@gmail.com>
2021-05-06 21:22:11 -0500

    added interval for fetching data

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,29 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Select from "react-select";
 import Table from "../Components/table";
 
-const options = [
-  { label: "Top", value: "topstories" },
-  { label: "Ask", value: "askstories" },
-  { label: "Show", value: "showstories" },
-  { label: "Job", value: "jobstories" },
-];
 export default function Home() {
   const [type, setType] = useState({ label: "Top", value: "topstories" });
   const [data, setData] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
   useEffect(() => {
-    setIsFetching(true);
-    axios
-      .get(`/api/hello/`, {
-        params: {
-          type: type.value,
-        },
-      })
-      .then((res) => {
-        setData(res.data.data);
-        setIsFetching(false);
-      });
+    const interval = setInterval(() => {
+      console.log(type);
+      setIsFetching(true);
+      axios
+        .get(`/api/hello/`, {
+          params: {
+            type: type.value,
+          },
+        })
+        .then((res) => {
+          setData(res.data.data);
+          setIsFetching(false);
+        });
+    }, 60 * 1000);
+    return () => clearInterval(interval);
   }, [type]);
 
   const onChangeOption = (option) => {
