a9a7a58099b3a3b442ef88b37be2cc02f67c3cfd
Vignesh KRV <vignesh1105krv@gmail.com>
2021-05-06 20:31:49 -0500

    created inital page and API

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+import axios from "axios";
+export default async (req, res) => {
+  res.statusCode = 200;
+  const promises = [];
+  const finalResult = [];
+  try {
+    if (req.method === "GET") {
+      let resp = await axios.get(
+        `https://hacker-news.firebaseio.com/v0/${req.query.type}.json?print=pretty`
+      );
+      let results = resp.data;
+      results.forEach((element) => {
+        promises.push(
+          axios.get(
+            `https://hacker-news.firebaseio.com/v0/item/${element}.json?print=pretty`
+          )
+        );
+      });
+      let data = await Promise.all(promises);
+      data.forEach((value, index) => {
+        finalResult.push(data[index].data);
+      });
+      res.status(200).json({ data: finalResult });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ data: error });
+  }
+};
